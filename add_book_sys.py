bb6780f6838e22595dc2e02d737ac5111c1ab97d
rohan vetale <vetalerohan@gmail.com>
2024-01-10 15:58:30 +0530

    [Rohan] Add: Program to normally get the user details for contact and print them

---
+++ add_book_sys.py
@@ -0,0 +1,21 @@
+'''
+
+@Author: Rohan Vetale
+
+@Date: 2024-01-10 09:00:30
+
+@Last Modified by: Rohan Vetale
+
+@Last Modified time: 2024-01-10 18:00:30
+
+@Title : Program to create a contact of a user normally
+
+''' 
+
+first_name = input("Enter your first name : ")
+last_name = input("Enter your last name : ")
+address = input("Enter your address in short : ")
+city_name = input("Enter your city name : ")
+zip_code = int(input("Enter your zip code : "))
+email_id = input("Enter your e-mail id : ")
+print(f"Contact is created with first name: {first_name}, last name: {last_name}, address: {address}, city: {city_name}, zip code: {zip_code}, and email id: {email_id} ")
