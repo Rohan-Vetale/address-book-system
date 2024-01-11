7e4d6cdf15e9a0e1d7af713f7f3a32de8160bf95
rohan vetale <vetalerohan@gmail.com>
2024-01-11 15:55:06 +0530

    [Rohan] Add: Program to use oops for address book

--- add_book_sys.py
+++ add_book_sys.py
@@ -12,10 +12,52 @@
 
 ''' 
 
-first_name = input("Enter your first name : ")
-last_name = input("Enter your last name : ")
-address = input("Enter your address in short : ")
-city_name = input("Enter your city name : ")
-zip_code = int(input("Enter your zip code : "))
-email_id = input("Enter your e-mail id : ")
-print(f"Contact is created with first name: {first_name}, last name: {last_name}, address: {address}, city: {city_name}, zip code: {zip_code}, and email id: {email_id} ")
+class Contact:
+    def __init__(self, first_name, last_name, address, city, state, phone_num, e_mail) :
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone_num = phone_num
+        self.e_mail = e_mail
+
+        
+class AddressBook :
+    def __init__(self) :
+        self.contacts = []
+    def add_contact(self, contact_obj):
+        self.contacts.append(contact_obj)
+        print("Contact added successfully")
+        
+    def display_contacts(self):
+        if len(self.contacts) == 0:
+            print("No contacts found.")
+            return
+        for contact in self.contacts:
+            print("First Name:", contact.first_name)
+            print("Last Name:", contact.last_name)
+            print("Address:", contact.address)
+            print("City:", contact.city)
+            print("State:", contact.state)
+            print("ZIP Code:", contact.zip_code)
+            print("Phone Number:", contact.phone_num)
+            print("Email:", contact.e_mail)
+            print("--------------------")
+        
+
+if __name__ == "__main__":
+    first_name = input("Enter your first name : ")
+    last_name = input("Enter your last name : ")
+    address = input("Enter your address in short : ")
+    city_name = input("Enter your city name : ")
+    state = input("Enter your state name : ")
+    phone_num = int(input("Enter your phone_num : "))
+    zip_code = int(input("Enter your zip code : "))
+    e_mail = input("Enter your e-mail id : ")
+    add_book_obj = AddressBook()
+    contact_obj = Contact(first_name, last_name, address, city_name, state, phone_num, e_mail)
+    add_book_obj.add_contact(contact_obj)
+    add_book_obj.display_contacts()
+    
