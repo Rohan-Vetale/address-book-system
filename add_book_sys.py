18d1ae6298758de273293566a9a8749302685295
rohan vetale <vetalerohan@gmail.com>
2024-01-12 17:06:01 +0530

    [Rohan] Add: Program to add multiple address books at a time

--- add_book_sys.py
+++ add_book_sys.py
@@ -31,6 +31,7 @@ class Contact:
         """
         while True:
             choice = int(input("""
+                        ----Choose one of the options below----
                         1. change first name
                         2. change last name
                         3. change address
@@ -90,7 +91,16 @@ class AddressBook:
         Return:None
         """
         for key, value in self.contact_dict.items():
-            print(f"first name: {key} last name: {value.last_name} phone number: {value.phone} ")
+            print(f"""
+                    first name: {key} 
+                    last name: {value.last_name} 
+                    phone number: {value.phone}
+                    address : {value.address}
+                    city : {value.city}
+                    state : {value.state}
+                    email : {value.email}
+                    Zip code : {value.pin}
+            """)
 
     def contact_update(self, name):
         """
@@ -115,8 +125,29 @@ class AddressBook:
         else:
             print("contact not found!!")
 
+
 class MegaBook:
-    pass
+
+    def __init__(self):
+        self.book_dict = {}
+
+    def add_multiple_book(self, addressbook_obj):
+        """
+        Description: This function add multiple book.
+        Parameter: address book object
+        Return:None
+        """
+        self.book_dict.update({addressbook_obj.address_book_name: addressbook_obj})
+        print(f"{addressbook_obj.address_book_name}")
+
+    def get_book(self, name):
+        """
+        Description: This function for getting the name of address book from address book dictionary .
+        Parameter: string
+        Return:name of address book present in address book dictionary.
+        """
+        return self.book_dict.get(name)
+
 
 def main():
     """
@@ -124,11 +155,11 @@ def main():
     Parameter: None
     Return:None
     """
-    address_book_name = input("Enter the book name: ")
-    addressbook_obj = AddressBook(address_book_name)
+    multiple_book_obj = MegaBook()
     try:
         while True:
             choice = int(input("""
+                        ----Choose one of the options below----
                         1. Add contact
                         2. get all details of contact
                         3. update contact info
@@ -137,6 +168,10 @@ def main():
             """))
             match choice:
                 case 1:
+                    address_book_name = input("Enter the book name: ")
+                    addressbook_obj = multiple_book_obj.get_book(address_book_name)
+                    if addressbook_obj is None:
+                        addressbook_obj = AddressBook(address_book_name)
                     first_name = input("Enter First Name: ")
                     last_name = input("Enter Last Name: ")
                     address = input("Enter Address: ")
@@ -149,12 +184,19 @@ def main():
                                             "city": city, "state": state, "pin": pin, "phone": phone, "email": email}
                     contact_obj = Contact(contact_detalis_dict)
                     addressbook_obj.add_contact(contact_obj)
+                    multiple_book_obj.add_multiple_book(addressbook_obj)
                 case 2:
+                    address_book_name = input("Enter the book name: ")
+                    addressbook_obj = multiple_book_obj.get_book(address_book_name)
                     addressbook_obj.contact_details()
                 case 3:
-                    name = input("Enter name: ")
+                    address_book_name = input("Enter the book name: ")
+                    addressbook_obj = multiple_book_obj.get_book(address_book_name)
+                    name = input("Enter name of contact: ")
                     addressbook_obj.contact_update(name)
                 case 4:
+                    address_book_name = input("Enter the book name: ")
+                    addressbook_obj = multiple_book_obj.get_book(address_book_name)
                     name = input("Enter name: ")
                     addressbook_obj.delete_contact(name)
                 case 5:
