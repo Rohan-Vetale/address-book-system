8f05e40aa9e0c01e8ce347f65d95569d27c6abd2
rohan vetale <vetalerohan@gmail.com>
2024-01-12 10:27:07 +0530

    [Rohan] Add: To edit a contact if it exists

--- add_book_sys.py
+++ add_book_sys.py
@@ -13,20 +13,23 @@
 ''' 
 
 class Contact:
-    def __init__(self, first_name, last_name, address, city, state, phone_num, e_mail) :
-        self.first_name = first_name
-        self.last_name = last_name
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip_code = zip_code
-        self.phone_num = phone_num
-        self.e_mail = e_mail
+        
+    def new_contact(self) :
+        self.first_name = input("Enter your first name : ")
+        self.last_name = input("Enter your last name : ")
+        self.address = input("Enter your address in short : ")
+        self.city = input("Enter your city name : ")
+        self.state = input("Enter your state name : ")
+        self.phone_num = int(input("Enter your phone_num : "))
+        self.zip_code = int(input("Enter your zip code : "))
+        self.e_mail = input("Enter your e-mail id : ")
+        
 
         
 class AddressBook :
     def __init__(self) :
         self.contacts = []
+        
     def add_contact(self, contact_obj):
         self.contacts.append(contact_obj)
         print("Contact added successfully")
@@ -36,6 +39,7 @@ class AddressBook :
             print("No contacts found.")
             return
         for contact in self.contacts:
+            print("----------------------------- \n ")
             print("First Name:", contact.first_name)
             print("Last Name:", contact.last_name)
             print("Address:", contact.address)
@@ -45,19 +49,48 @@ class AddressBook :
             print("Phone Number:", contact.phone_num)
             print("Email:", contact.e_mail)
             print("--------------------")
+    
+    def edit_contact(self):
+        first_name = input("Enter the first name to search : ")
+        last_name = input("Enter the last name to search : ")
+        for contact in self.contacts:
+            if contact.first_name == first_name and contact.last_name == last_name:
+                new_address = input("Enter new address: ")
+                new_city = input("Enter new city: ")
+                new_state = input("Enter new state: ")
+                new_zip_code = input("Enter new ZIP code: ")
+                new_phone_number = input("Enter new phone number: ")
+                new_email = input("Enter new email: ")
+                contact.address = new_address
+                contact.city = new_city
+                contact.state = new_state
+                contact.zip_code = new_zip_code
+                contact.phone_num = new_phone_number
+                contact.e_mail = new_email
+                print("Contact updated successfully.")
+                return
+        print("Contact not found.")
+        
+    
         
 
 if __name__ == "__main__":
-    first_name = input("Enter your first name : ")
-    last_name = input("Enter your last name : ")
-    address = input("Enter your address in short : ")
-    city_name = input("Enter your city name : ")
-    state = input("Enter your state name : ")
-    phone_num = int(input("Enter your phone_num : "))
-    zip_code = int(input("Enter your zip code : "))
-    e_mail = input("Enter your e-mail id : ")
-    add_book_obj = AddressBook()
-    contact_obj = Contact(first_name, last_name, address, city_name, state, phone_num, e_mail)
-    add_book_obj.add_contact(contact_obj)
-    add_book_obj.display_contacts()
+    
+    addr_book_obj = AddressBook()
+    while True:
+
+        choice_num = int(input("Enter the choice number you want for: \n 1. Add New Contact \n 2. Edit a contact \n --> "))
+
+        match choice_num:
+            case 1:
+                contact_obj = Contact()
+                contact_obj.new_contact()
+                addr_book_obj.add_contact(contact_obj=contact_obj)
+                addr_book_obj.display_contacts()
+            case 2:
+                addr_book_obj.edit_contact()
+                addr_book_obj.display_contacts()
+            case 10:
+                break
+    
     
